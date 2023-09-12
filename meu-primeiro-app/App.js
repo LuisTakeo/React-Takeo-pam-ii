7cbc2ee2c812d59ef6f07426c20902352302ced0
LuisTakeo <luistakeo@hotmail.com>
2023-09-11 22:31:40 -0300

    Contador em React-Native

---
+++ meu-primeiro-app/App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Contador from './components/Contador';
+
+export default function App() {
+  return (
+    <View>
+      <Contador/>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
